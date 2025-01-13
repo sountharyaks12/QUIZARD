5f431c36e80aba41cbf5d4027727e30cdcdd11eb
sountharya <sountharyaks@gmail.com>
2025-01-13 19:29:35 +0530

    1st pg

--- src/Components/FirstPage.jsx
+++ src/Components/FirstPage.jsx
@@ -6,13 +6,13 @@ export default function FirstPage() {
 
     return (
         <div className="bg-darkpurple-0 h-screen font-rubik text-white relative">
-            <h1 className="text-5xl sm:text-5xl pt-12 font-bold text-center">
+            <h1 className="text-5xl sm:text-5xl pt-14 font-bold text-center">
                 Quizard
             </h1>
             <div className="flex justify-center items-center flex-wrap pt-6 px-20 text-center">
                 <p>1,000+ quizzes to challenge you and your friends on all topics!</p>
             </div>
-            <div className="relative flex justify-center items-center ">
+            <div className="relative flex justify-center items-center  ">
                 <div>
                     <div className="blob1"></div>
                     <div className="blob2"></div>
@@ -20,11 +20,10 @@ export default function FirstPage() {
                 <img className="treasureBox w-auto" src="img2.png" alt="Treasure box" />
 
             </div>
-            <div className="flex w-screen justify-center items-center mt-36 p-10">
+            <div className="flex w-full justify-center items-center mt-20 p-4">
                 <button
-                    className="bg-cyan-400 text-indigo-950 font-medium px-7 py-2 rounded-lg"
-                    onClick={() => navigate("/secondpage")} 
-                >
+                    className="bg-cyan-400 text-darkpurple-0 font-bold  py-2 rounded-lg w-9/12"
+                    onClick={() => navigate("/secondpage")}>
                     Start playing
                 </button>
             </div>
